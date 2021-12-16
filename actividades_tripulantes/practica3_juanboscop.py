2f15282dca252a4115ee77cf94a014d6f96449f8
juanboscop <juanbosco.pavia@gmail.com>
2021-12-16 18:57:43 +0100

    SFII_Actividades_tripulantes

---
+++ actividades_tripulantes/practica3_juanboscop.py
@@ -0,0 +1,8 @@
+#welcome to the list converter.
+print("\n\t\t-----> Welcome to bosco's list changer <-----\t\t\n")
+listn = [1,2,3,4,5,6,7,8,9,10]
+print(f"\nThis is the list without changes ===> {listn}\n")
+listn[4] *= 2
+listn[7] *= 2
+listn[9] *= 2
+print(f"\nThis is the modified list ===> {listn}\n")
---
+++ actividades_tripulantes/practica8.py
@@ -0,0 +1,1 @@
+#practica 8 es subir todo esto a git hub no?
